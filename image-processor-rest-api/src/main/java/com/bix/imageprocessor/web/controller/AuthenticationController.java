bf71029e8e7e1a7b2bebc7ca28398e9b069101e8
Gustavo Porto <gustavofrancoporto@toptal.com>
2025-03-29 10:29:48 -0300

    Simplifying auth response.

--- image-processor-rest-api/src/main/java/com/bix/imageprocessor/web/controller/AuthenticationController.java
+++ image-processor-rest-api/src/main/java/com/bix/imageprocessor/web/controller/AuthenticationController.java
@@ -3,10 +3,8 @@ package com.bix.imageprocessor.web.controller;
 import com.bix.imageprocessor.domain.user.service.UserService;
 import com.bix.imageprocessor.security.service.JwtTokenService;
 import com.bix.imageprocessor.web.dto.auth.AuthRequestDto;
-import com.bix.imageprocessor.web.dto.auth.AuthResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -30,16 +28,16 @@ public class AuthenticationController {
     @PostMapping("/login")
     @Operation(summary = "Get access token given the credentials")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Authenticated successfully", content = {@Content(mediaType = "application/json", schema = @Schema(implementation = AuthResponseDto.class))}),
+            @ApiResponse(responseCode = "200", description = "Token generated", content = @Content),
             @ApiResponse(responseCode = "400", description = "Invalid credentials", content = @Content)
     })
-    public AuthResponseDto authenticate(@RequestBody @Validated AuthRequestDto authRequestDto) {
+    public String authenticate(@RequestBody @Validated AuthRequestDto authRequestDto) {
 
         var user = userService.authenticate(authRequestDto.email(), authRequestDto.password())
                 .orElseThrow(() -> new BadCredentialsException("Invalid email or password"));
 
         var token = jwtTokenService.createToken(user);
 
-        return new AuthResponseDto(token.getTokenValue());
+        return token.getTokenValue();
     }
 }
