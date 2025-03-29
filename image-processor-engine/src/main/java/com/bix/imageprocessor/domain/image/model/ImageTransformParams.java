c6063d3f975532b33797c072ec4678f7d4926fe5
Gustavo Porto <gustavofrancoporto@toptal.com>
2025-03-28 21:57:43 -0300

    Cleanup

--- image-processor-engine/src/main/java/com/bix/imageprocessor/domain/image/model/ImageTransformParams.java
+++ image-processor-engine/src/main/java/com/bix/imageprocessor/domain/image/model/ImageTransformParams.java
@@ -1,14 +1,4 @@
 package com.bix.imageprocessor.domain.image.model;
 
-import java.math.BigDecimal;
-//
-//public record ImageTransformParams(
-//        BigDecimal resizeRatio,
-//        Integer sepiaIntensity,
-//        Boolean grayscale,
-//        Boolean invertColors
-//) {
-//}
-
 public interface ImageTransformParams {
 }
