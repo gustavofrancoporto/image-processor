94e29965c69792db3b64bc46433d846f041f50b8
Gustavo Porto <gustavofrancoporto@toptal.com>
2025-03-31 17:43:34 -0300

    Change the set implementation to make sure the order of the roles are always the same and the test assertion can be made safely.

--- image-processor-rest-api/src/test/java/com/bix/imageprocessor/domain/user/model/UserTest.java
+++ image-processor-rest-api/src/test/java/com/bix/imageprocessor/domain/user/model/UserTest.java
@@ -2,7 +2,8 @@ package com.bix.imageprocessor.domain.user.model;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Set;
+import java.util.LinkedHashSet;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,8 +15,10 @@ public class UserTest {
         var adminRole = new Role(1L, "ADMIN");
         var userRole = new Role(2L, "USER");
 
+        var roles = new LinkedHashSet<>(List.of(adminRole, userRole));
+
         var user = new User();
-        user.setRoles(Set.of(adminRole, userRole));
+        user.setRoles(roles);
 
         var roleNames = user.getRoleNames();
 
